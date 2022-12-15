9e0ddf8634f000bd26bf2b214092caa5966ccea5
Adam Paquette <nfs120@gmail.com>
2022-12-14 21:50:27 -0500

    Shape the Api

--- src/FluentType.Playground/Program.cs
+++ src/FluentType.Playground/Program.cs
@@ -4,7 +4,46 @@ using System.Numerics;
 var builder = ValueObjectBuilder.Instance;
 
 builder.For<UserId>().As<int>().Length(1, 100);
-//builder.MinLength<UserId>(1, 100);
+
+/*
+Base validators:
+
+builder.NotEmpty<UserId>();
+builder.Length<UserId>(20, 250);
+builder.MinLength<UserId>(3);
+builder.MaxLength<UserId>(3);
+builder.LessThen<UserId>(3);
+builder.LessThanOrEqualTo<UserId>(3);
+builder.GreaterThan<UserId>(3);
+builder.GreaterThanOrEqualTo<UserId>(3);
+builder.Must<UserId>(BeAValidId);
+builder.Matches<UserId>("[0-9]{2}");
+builder.Equal<UserId>("abc")
+builder.NotEqual<UserId>("abc");
+builder.NotEqual<UserId>("abc", StringComparer.OrdinalIgnoreCase);
+builder.ExclusiveBetween<UserId>(1, 10);
+builder.InclusiveBetween<UserId>(1, 10);
+builder.PrecisionScale<UserId>(4, 2, false);
+builder.IsInEnum<UserId>(MyEnum); Usefull ??
+
+Combine validators:
+
+builder
+    .NotEmpty<UserId>()
+    .Length(20)
+    .Matches("[0-9]{20}");
+
+Extend validators:
+
+builder.MinLength<UserId>().WithMessage("Pleasy specify a {TypeName} with a minimum length of {MinLength}.");
+builder.For<UserId>().As<int>().Must(BeAValidEmail).WithMessage("Please enter a valid email.");
+
+
+Global configurations:
+
+builder.Global.AllowNull();
+builder.Global.AllowDefault();
+*/
 
 builder.For<FirstName>()
     .As<string>()
