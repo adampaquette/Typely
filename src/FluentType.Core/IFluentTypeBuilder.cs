3c46e415030933fe901103f4082429a3906ff0d3
Adam Paquette <nfs120@gmail.com>
2022-12-16 16:18:36 -0500

    Group function builders by type

--- src/FluentType.Core/IFluentTypeBuilder.cs
+++ src/FluentType.Core/IFluentTypeBuilder.cs
@@ -4,7 +4,7 @@ namespace FluentType.Core;
 
 public interface IFluentTypeBuilder
 {
-     IFluentTypeBuilder<T> For<T>(string typeName);
+    IFluentTypeBuilder<T> For<T>(string typeName);
 }
 
 public interface IFluentTypeBuilder<T>
@@ -14,22 +14,22 @@ public interface IFluentTypeBuilder<T>
     IFluentTypeBuilder<T> AsClass();
     IFluentTypeBuilder<T> AsRecord();
     IFluentTypeBuilder<T> WithName(string message);
-    
-    IRuleBuilder<T> NotEmpty();
-    IRuleBuilder<T> NotEqual(T value);    
-    IRuleBuilder<T> Length(int min, T max);
-    IRuleBuilder<T> Length(int exactLength);
-    IRuleBuilder<T> MinLength(int minLength);
-    IRuleBuilder<T> MaxLength(int maxLength);
-    IRuleBuilder<T> LessThan(int value);
-    IRuleBuilder<T> LessThanOrEqual(int value);
-    IRuleBuilder<T> GreaterThan(int value);
-    IRuleBuilder<T> GreaterThanOrEqual(int value);
-    IRuleBuilder<T> Must(Expression<Func<T, bool>> predicate);
-    IRuleBuilder<T> Matches(string regex);
-    IRuleBuilder<T> InclusiveBetween(T min, T max);
-    IRuleBuilder<T> ExclusiveBetween(T min, T max);
-    IRuleBuilder<T> PrecisionScale(int precision, int scale);
+
+    IRuleBuilder<T> NotEmpty(); //T
+    IRuleBuilder<T> NotEqual(T value); //T
+    IRuleBuilder<T> Must(Expression<Func<T, bool>> predicate); //T
+    IRuleBuilder<T> Length(int min, T max); //string
+    IRuleBuilder<T> Length(int exactLength); //string
+    IRuleBuilder<T> MinLength(int minLength); //string
+    IRuleBuilder<T> MaxLength(int maxLength); //string
+    IRuleBuilder<T> Matches(string regex); //string
+    IRuleBuilder<T> LessThan(T value); //IComparable
+    IRuleBuilder<T> LessThanOrEqual(T value); //IComparable
+    IRuleBuilder<T> GreaterThan(T value); //IComparable
+    IRuleBuilder<T> GreaterThanOrEqual(T value); //IComparable    
+    IRuleBuilder<T> InclusiveBetween(T min, T max); //INumber
+    IRuleBuilder<T> ExclusiveBetween(T min, T max); //INumber
+    IRuleBuilder<T> PrecisionScale(int precision, int scale); //INumber
 }
 
 public interface IRuleBuilder<T> : IFluentTypeBuilder<T>
