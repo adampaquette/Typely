5e7dd390bd547c9c38e920f6f76e3760d9b24b3d
Adam Paquette <nfs120@gmail.com>
2022-12-17 09:25:36 -0500

    cleanup old ganerator v1

--- src/FluentType.Generators/Extensions/TypeSymbolExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.CodeAnalysis;
-
-namespace FluentType.Generator.Extensions
-{
-    internal static class TypeSymbolExtensions
-    {
-        public static bool IsNullable(this ITypeSymbol typeSymbol) => typeSymbol.Name == "Nullable";
-    }
-}
