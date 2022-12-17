b35afcc42b29aab158cc240cd7bef9ee6ce3d857
Adam Paquette <nfs120@gmail.com>
2022-12-16 22:40:22 -0500

    IIncrementalGenerator

--- src/FluentType.Generator/Generators/FluentTypeGenerator.cs
+++ src/FluentType.Generator/Generators/FluentTypeGenerator.cs
@@ -1,19 +1,55 @@
 ﻿using FluentType.Core;
-using FluentType.SourceGenerators.Commun;
+using FluentType.SourceGenerators.Extensions;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using Scriban.Parsing;
+using System.Collections.Immutable;
+using System.Text;
 
 namespace FluentType.SourceGenerators.Generators;
 
 [Generator]
-public class FluentTypeGenerator : ISourceGenerator
+public class FluentTypeGenerator : IIncrementalGenerator
 {
-    public void Initialize(GeneratorInitializationContext context)
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        context.RegisterForSyntaxNotifications(() => new ImplementsInterfaceSyntaxReceiver<IFluentTypesConfiguration>());
+        var classDeclarations = context.SyntaxProvider
+            .CreateSyntaxProvider(
+                predicate: static (x, _) => IsSyntaxTargetForGeneration(x),
+                transform: static (ctx, _) => GetSemanticTargetForGeneration(ctx))
+            .Where(static m => m is not null);
+
+        var compilationAndClasses = context.CompilationProvider.Combine(classDeclarations.Collect());
+
+        context.RegisterSourceOutput(compilationAndClasses, static (spc, source) => Execute(source.Item1, source.Item2, spc));
+
     }
 
-    public void Execute(GeneratorExecutionContext context)
+    static bool IsSyntaxTargetForGeneration(SyntaxNode syntaxNode) =>
+        syntaxNode is ClassDeclarationSyntax c && c.HasInterface(nameof(IFluentTypesConfiguration));
+
+    static ClassDeclarationSyntax? GetSemanticTargetForGeneration(GeneratorSyntaxContext context)
     {
-        
+        var classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
+        var classSymbol = context.SemanticModel.GetSymbolInfo(classDeclarationSyntax).Symbol as INamedTypeSymbol;
+        if (classSymbol == null)
+        {
+            return null;
+        }
+
+        return classDeclarationSyntax;
+    }
+
+    private static void Execute(Compilation compilation, ImmutableArray<ClassDeclarationSyntax> classes, SourceProductionContext context)
+    {
+        if (classes.IsDefaultOrEmpty)
+        {
+            return;
+        }
+
+        IEnumerable<ClassDeclarationSyntax> distinctClasses = classes.Distinct();
+
+
     }
 }
