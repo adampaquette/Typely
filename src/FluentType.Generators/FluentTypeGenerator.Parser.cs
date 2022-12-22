3373c152d4ce9215ad5bbb81e14a57272a0ef235
Adam Paquette <nfs120@gmail.com>
2022-12-21 19:43:18 -0500

    r#

--- src/FluentType.Generators/FluentTypeGenerator.Parser.cs
+++ src/FluentType.Generators/FluentTypeGenerator.Parser.cs
@@ -6,7 +6,6 @@ using Microsoft.CodeAnalysis.CSharp;
 using System.Reflection;
 using FluentType.Generators.Extensions;
 using Basic.Reference.Assemblies;
-using Microsoft.Extensions.DependencyModel;
 
 namespace FluentType.Generators;
 
@@ -55,12 +54,58 @@ public partial class FluentTypeGenerator
                 // Stop if we're asked to
                 _cancellationToken.ThrowIfCancellationRequested();
 
-                var syntaxTree = group.Key;
+                //var syntaxTree = group.Key;
+
+                var source = """
+                using System;
+                using FluentType.Core;                
+
+                namespace Test;
+
+                public class A : IFluentTypeBuilder
+                {
+                }
+                """;
+                var syntaxTree = SyntaxFactory.ParseSyntaxTree(source);
+
+               
+
                 var compilation = CSharpCompilation.Create(assemblyName: Path.GetRandomFileName())
                     .WithReferenceAssemblies(ReferenceAssemblyKind.NetStandard20)
-                    .AddReferences(typeof(IFluentTypesConfiguration))
+                    //.AddReferences(typeof(IFluentTypesConfiguration))
                     .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
                     .AddSyntaxTrees(syntaxTree);
+
+                var types2 = typeof(IFluentTypesConfiguration).Assembly.GetTypes();
+
+                compilation = compilation.AddReferences(MetadataReference.CreateFromFile("C:\\Users\\nfs12\\source\\repos\\FluentType\\src\\FluentType.Generators\\FluentType.Core.dll"));
+                //compilation = compilation.AddReferences(MetadataReference.CreateFromFile(typeof(System.Runtime.CompilerServices.AccessedThroughPropertyAttribute).Assembly.Location));
+
+                var a = typeof(IFluentTypesConfiguration).Assembly.Location;
+
+                var refs = compilation.References.ToList();
+
+                ////DOMAIN ASSEMBLIES
+                //var domainAssemblies = AppDomain.CurrentDomain.GetAssemblies()
+                //    .Where(x => !x.IsDynamic)
+                //    .Select(x => MetadataReference.CreateFromFile(x.Location))
+                //    .ToList();
+                //compilation = compilation.AddReferences(domainAssemblies);
+
+                ////REFERENCE ASSEMBLIES
+                //var netstandardFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".nuget\\packages\\netstandard.library\\2.0.3\\build\\netstandard2.0\\ref");
+                //var referenceAssemblies = Directory.GetFiles(netstandardFolder)
+                //    .Where(x => x.EndsWith(".dll"))
+                //    .Select(p => MetadataReference.CreateFromFile(p))
+                //    .ToList();
+                //compilation = compilation.AddReferences(referenceAssemblies);
+
+                ////IMPLEMENTATION ASSEMBLIES
+                //var implementationAssemblies = ((string)AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES")).Split(Path.PathSeparator);
+                //var references = implementationAssemblies
+                //    .Select(p => MetadataReference.CreateFromFile(p))
+                //    .ToList();
+                //compilation = compilation.AddReferences(references);     
 
                 using (var ms = new MemoryStream())
                 {
@@ -71,22 +116,23 @@ public partial class FluentTypeGenerator
                     }
 
                     ms.Seek(0, SeekOrigin.Begin);
+
+
                     var assembly = Assembly.Load(ms.ToArray());
 
                     try
                     {
                         var types = assembly.GetTypes();
+                        dynamic fluentTypeConfiguration = assembly.CreateInstance("Test.TypesConfiguration");
+
+                        //var fluentBuilder = new FluentTypeBuilder();
+                        //fluentTypeConfiguration.Configure();
+                        //var called = fluentBuilder.Called;
                     }
                     catch (Exception ex)
                     {
                         var gga = ex;
                     }
-
-                    dynamic fluentTypeConfiguration = assembly.CreateInstance("FluentType.Playground.TypesConfiguration");
-
-                    var fluentBuilder = new FluentTypeBuilder();
-                    fluentTypeConfiguration.Configure();
-                    var called = fluentBuilder.Called;
                 }
             }
 
@@ -98,14 +144,14 @@ public partial class FluentTypeGenerator
     {
     }
 
-    internal class FluentTypeBuilder : IFluentTypeBuilder
-    {
-        public int Called { get; set; }
+    //internal class FluentTypeBuilder : IFluentTypeBuilder
+    //{
+    //    public int Called { get; set; }
 
-        public IFluentTypeBuilder<T> For<T>(string typeName)
-        {
-            Called++;
-            return default;
-        }
-    }
+    //    public IFluentTypeBuilder<T> For<T>(string typeName)
+    //    {
+    //        Called++;
+    //        return default;
+    //    }
+    //}
 }
--- src/FluentType.Playground/TypesConfiguration.cs
+++ src/FluentType.Playground/TypesConfiguration.cs
@@ -1,22 +1,10 @@
 ﻿using FluentType.Core;
 
-namespace FluentType.Playground;
+namespace Test;
 
 public class TypesConfiguration : IFluentTypesConfiguration
 {
     public void Configure(IFluentTypeBuilder builder)
     {
-        builder.For<int>("Likes");
-        //builder.For<decimal>("Rating").InclusiveBetween(0, 5);
-        //builder.For<string>("FirstName").NotEmpty();
-
-        //builder
-        //    .For<int>("UserId")
-        //    .Namespace("MyDomain")
-        //    .AsStruct()
-        //    .Length(20)
-        //    .WithMessage("Pleasy specify a {Name} with a length of {Length}.")
-        //    .Matches("[0-9]{5}[a-Z]{15}")
-        //    .WithName("user identifier");
     }
 }
