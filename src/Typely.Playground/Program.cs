c0fcb65386632d2775558d72d0b119949f7dd0a7
Adam Paquette <nfs120@gmail.com>
2022-12-27 10:15:47 -0500

    Build props for nugets

--- src/Typely.Playground/Program.cs
+++ src/Typely.Playground/Program.cs
@@ -1,22 +1,7 @@
 ﻿
 
 var a = "";
-
-//public struct Age
-//{ 
-//    //private Age() { }
-//    public Age(int value) { }
-//}
-
-//public readonly record struct UserId
-//{
-//    //public UserId() { }
-
-//    public UserId(int value)
-//    { 
-
-//    }
-//}
+a += "a";
 
 //Global configurations:
 
