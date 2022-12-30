8977913cd1e8e9326c93e8044eb083ba15fb0f18
Adam Paquette <nfs120@gmail.com>
2022-12-30 09:53:09 -0500

    BadLanguageConstructs

---
+++ src/Typely.Tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace Typely.Tests;
+
+public class Tests
+{
+    [Fact]
+    public void BadLanguageConstructs()
+    {
+        var a = new MyStruct();
+        var b = a with { Value = 8 };
+        var c = default(MyStruct);
+    }
+}
+
+public struct MyStruct
+{
+    public int Value { get; set; }
+}
