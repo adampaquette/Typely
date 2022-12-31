b7d110c8f2f77e089041a9d26f5ba15094b6c04c
Adam Paquette <nfs120@gmail.com>
2022-12-30 19:55:15 -0500

    TypelyValue.TryFrom

--- src/Typely.POC/ReferenceSample.cs
+++ src/Typely.POC/ReferenceSample.cs
@@ -1,33 +1,41 @@
-﻿using System.Resources;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Resources;
 using Typely.Core;
 using Typely.POC;
 
 namespace Typely.Tests;
 
-public struct Value
-{ 
+public static class TypelyValue
+{
+    public static void ValidateAndThrow<TValue, TThis>(TValue value) where TThis : IValue<TValue, TThis>
+    {
+        var validationError = TThis.Validate(value);
+        if (validationError != null)
+        {
+            throw new ArgumentException(validationError.ToString()); //Comment la désérialisation Json va fonctionner?
+        }
+    }
 
+    public static bool TryFrom<TValue, TThis>(TValue value, [MaybeNullWhen(false)] out TThis instance, out ValidationError? validationError)
+        where TThis : IValue<TValue, TThis>
+    {
+        validationError = TThis.Validate(value);
+        var isValid = validationError != null;
+        instance = isValid ? TThis.From(value) : default;
+        return isValid;
+    }
 }
 
-public readonly partial struct ReferenceSample : IValue<int, ReferenceSample>
+public partial struct ReferenceSample : IValue<int, ReferenceSample>
 {
-    public int Value { get; }
+    public int Value { get; private set; }
 
     public ReferenceSample() => throw new Exception("Parameterless constructor not accessible.");
 
     public ReferenceSample(int value)
     {
-        ValidateAndThrow(value);
+        TypelyValue.ValidateAndThrow<int, ReferenceSample>(value);
         Value = value;
-    }
-    
-    public static void ValidateAndThrow(int value)
-    {
-        var validationError = Validate(value);
-        if (validationError != null)
-        {
-            throw new ArgumentException(validationError.ToString()); //Comment la désérialisation Json va fonctionner?
-        }
     }
 
     public static ValidationError? Validate(int value)
@@ -45,11 +53,17 @@ public readonly partial struct ReferenceSample : IValue<int, ReferenceSample>
     {
         validationError = Validate(value);
         var isValid = validationError != null;
-        instance = isValid ? new(value) : default;
+        instance = default;
+        if (isValid)
+        {
+            // Bypass constructor to avoid validating again
+            instance.Value = value;
+        }
         return isValid;
     }
 
-
+    public static bool TryFrom2(int value, out ReferenceSample instance, out ValidationError? validationError) =>
+        TypelyValue.TryFrom(value, out instance, out validationError);
 }
 
 
