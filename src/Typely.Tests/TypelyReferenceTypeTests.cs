fa6ed9d57d24569d2d5f94c8373c1fe8633e07d7
Adam Paquette <nfs120@gmail.com>
2023-01-02 09:55:39 -0500

    Equality tests

---
+++ src/Typely.Tests/TypelyReferenceTypeTests.cs
@@ -0,0 +1,13 @@
+﻿using CsCheck;
+
+namespace Typely.Tests;
+
+public class TypelyReferenceTypeTests
+{
+    [Fact] public void Equals_ShouldBe_True() => Gen.String.Select(x => (ReferenceType.From(x), ReferenceType.From(x))).Sample((x, y) => x.Equals(y));
+    [Fact] public void Equals_ShouldBe_False() => Gen.String.Select(x => (ReferenceType.From(x), ReferenceType.From(x + 1))).Sample((x, y) => !x.Equals(y));
+    [Fact] public void OperatorEqual_ShouldBe_True() => Gen.String.Select(x => (ReferenceType.From(x), ReferenceType.From(x))).Sample((x, y) => x == y);
+    [Fact] public void OperatorEqual_ShouldBe_False() => Gen.String.Select(x => (ReferenceType.From(x), ReferenceType.From(x + 1))).Sample((x, y) => !(x == y));
+    [Fact] public void OperatorNotEqual_ShouldBe_True() => Gen.String.Select(x => (ReferenceType.From(x), ReferenceType.From(x))).Sample((x, y) => !(x != y));
+    [Fact] public void OperatorNotEqual_ShouldBe_False() => Gen.String.Select(x => (ReferenceType.From(x), ReferenceType.From(x + 1))).Sample((x, y) => x != y);
+}
---
+++ src/Typely.Tests/TypelyValueTypeTests.cs
@@ -0,0 +1,13 @@
+﻿using CsCheck;
+
+namespace Typely.Tests;
+
+public class TypelyValueTypeTests
+{
+    [Fact] public void Equals_ShouldBe_True() => Gen.Int.Select(x => (ValueType.From(x), ValueType.From(x))).Sample((x, y) => x.Equals(y));
+    [Fact] public void Equals_ShouldBe_False() => Gen.Int.Select(x => (ValueType.From(x), ValueType.From(x + 1))).Sample((x, y) => !x.Equals(y));
+    [Fact] public void OperatorEqual_ShouldBe_True() => Gen.Int.Select(x => (ValueType.From(x), ValueType.From(x))).Sample((x, y) => x == y);
+    [Fact] public void OperatorEqual_ShouldBe_False() => Gen.Int.Select(x => (ValueType.From(x), ValueType.From(x + 1))).Sample((x, y) => !(x == y));
+    [Fact] public void OperatorNotEqual_ShouldBe_True() => Gen.Int.Select(x => (ValueType.From(x), ValueType.From(x))).Sample((x, y) => !(x != y));
+    [Fact] public void OperatorNotEqual_ShouldBe_False() => Gen.Int.Select(x => (ValueType.From(x), ValueType.From(x + 1))).Sample((x, y) => x != y);
+}
