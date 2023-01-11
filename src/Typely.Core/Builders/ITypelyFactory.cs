b79282387159191299bbd0ae5b4055b57168cd63
Adam Paquette <nfs120@gmail.com>
2023-01-10 22:31:35 -0500

    ITypelyFactory internal doc

--- src/Typely.Core/Builders/ITypelyFactory.cs
+++ src/Typely.Core/Builders/ITypelyFactory.cs
@@ -8,10 +8,43 @@ namespace Typely.Core.Builders;
 /// <typeparam name="TBuilder">Type of the value object builder.</typeparam>
 public interface ITypelyFactory<TBuilder>
 {
+    /// <summary>
+    /// Type to generator.
+    /// </summary>
+    /// <param name="typeName">Name of the type.</param>
+    /// <returns>Fluent <see cref="TBuilder"/>.</returns>
     TBuilder For(string typeName);
+
+    /// <summary>
+    /// Make the generated type a struct.
+    /// </summary>
+    /// <returns>Fluent <see cref="TBuilder"/>.</returns>
     TBuilder AsStruct();
+
+    /// <summary>
+    /// Make the generated type a class.
+    /// </summary>
+    /// <returns>Fluent <see cref="TBuilder"/>.</returns>
     TBuilder AsClass();
+
+    /// <summary>
+    /// Namespace associated to the generate type.
+    /// </summary>
+    /// <param name="value">Namespace</param>
+    /// <returns>Fluent <see cref="TBuilder"/>.</returns>
     TBuilder WithNamespace(string value);
+
+    /// <summary>
+    /// Name used in the validation error message.
+    /// </summary>
+    /// <param name="name">User friendly name for the type.</param>
+    /// <returns>Fluent <see cref="TBuilder"/>.</returns>
     TBuilder WithName(string name);
+
+    /// <summary>
+    /// Name used in the validation error message. Supports localization.
+    /// </summary>
+    /// <param name="expression">An expression to be evaluated at runtime to get the name.</param>
+    /// <returns>Fluent <see cref="TBuilder"/>.</returns>
     TBuilder WithName(Expression<Func<string>> expression);
 }
