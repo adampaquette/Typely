b4ae2cd4f024c572329bed934b79a513d13bf6d3
Adam Paquette <nfs120@gmail.com>
2023-01-14 23:00:21 -0500

    TypelyString

--- src/Typely.Tests/String/TypelyStringTests.cs
+++ src/Typely.Tests/String/TypelyStringTests.cs
@@ -2,7 +2,7 @@
 
 namespace Typely.Tests.String;
 
-public class TypelyReferenceTypeTests
+public class TypelyStringTests
 {
     [Fact] public void Equals_ShouldBe_True() => GenTrueEquals.Sample((x, y) => x.Equals(y));
     [Fact] public void Equals_ShouldBe_False() => GenFalseEquals.Sample((x, y) => !x.Equals(y));
@@ -13,7 +13,7 @@ public class TypelyReferenceTypeTests
     [Fact] public void CompareTo() => GenComparable.Sample((x) => x.primitive.CompareTo(x.randomObj.Value) == x.valueObject.CompareTo(x.randomObj));
     [Fact] public void CompareToObject() => GenComparable.Sample((x) => x.primitive.CompareTo((object)x.randomObj.Value) == x.valueObject.CompareTo((object)x.randomObj));
 
-    private Gen<(StringType, StringType)> GenTrueEquals => Gen.String.Select(x => (StringType.From(x), StringType.From(x)));
-    private Gen<(StringType, StringType)> GenFalseEquals => Gen.String.Select(x => (StringType.From(x), StringType.From(x + "1")));
-    private Gen<(string primitive, StringType valueObject, StringType randomObj)> GenComparable => Gen.Select(Gen.String, Gen.String, (x, y) => (x, StringType.From(x), StringType.From(y)));
+    private Gen<(TypelyString, TypelyString)> GenTrueEquals => Gen.String.Select(x => (TypelyString.From(x), TypelyString.From(x)));
+    private Gen<(TypelyString, TypelyString)> GenFalseEquals => Gen.String.Select(x => (TypelyString.From(x), TypelyString.From(x + "1")));
+    private Gen<(string primitive, TypelyString valueObject, TypelyString randomObj)> GenComparable => Gen.Select(Gen.String, Gen.String, (x, y) => (x, TypelyString.From(x), TypelyString.From(y)));
 }
