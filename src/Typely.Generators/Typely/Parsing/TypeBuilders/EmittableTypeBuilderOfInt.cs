67fe8ea3aa1707de01f98f7053b4ed6fd34f6730
Adam Paquette <nfs120@gmail.com>
2023-04-15 14:08:31 -0400

    Simplify emittable type builder

--- src/Typely.Generators/Typely/Parsing/TypeBuilders/EmittableTypeBuilderOfInt.cs
+++ src/Typely.Generators/Typely/Parsing/TypeBuilders/EmittableTypeBuilderOfInt.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.CodeAnalysis;
-using Typely.Core;
-using Typely.Core.Builders;
-using Typely.Generators.Typely.Emitting;
 
 namespace Typely.Generators.Typely.Parsing.TypeBuilders;
 
@@ -20,64 +17,8 @@ internal class EmittableTypeBuilderOfInt : EmittableTypeBuilderBase, IEmittableT
             {
                 continue;
             }
-
-            switch (invocation.MemberName)
-            {
-                case nameof(ITypelyBuilderOfInt.NotEmpty):
-                    AddRule(
-                        errorCode: ErrorCodes.NotEmpty,
-                        rule: $"{Emitter.ValueParameterName} == default",
-                        message: nameof(ErrorMessages) + "." + nameof(ErrorMessages.NotEmpty));
-                    break;
-                case nameof(ITypelyBuilderOfInt.Must):
-                    var value = invocation.GetLambdaBodyOfFirstArgument();
-                    AddRule(
-                        errorCode: ErrorCodes.Must,
-                        rule: $"!({value})",
-                        message: nameof(ErrorMessages) + "." + nameof(ErrorMessages.Must));
-                    break;
-                case nameof(ITypelyBuilderOfInt.GreaterThan):
-                    value = invocation.GetFirstNumberArgument();
-                    AddRule(
-                        errorCode: ErrorCodes.GreaterThan,
-                        rule: $"{Emitter.ValueParameterName} <= {value}",
-                        message: nameof(ErrorMessages) + "." + nameof(ErrorMessages.GreaterThan),
-                        placeholders: (ValidationPlaceholders.ComparisonValue, value));
-                    break;
-                case nameof(ITypelyBuilderOfInt.GreaterThanOrEqualTo):
-                    value = invocation.GetFirstNumberArgument();
-                    AddRule(
-                        errorCode: ErrorCodes.GreaterThanOrEqualTo,
-                        rule: $"{Emitter.ValueParameterName} < {value}",
-                        message: nameof(ErrorMessages) + "." + nameof(ErrorMessages.GreaterThanOrEqualTo),
-                        placeholders: (ValidationPlaceholders.ComparisonValue, value));
-                    break;
-                case nameof(ITypelyBuilderOfInt.LessThan):
-                    value = invocation.GetFirstNumberArgument();
-                    AddRule(
-                        errorCode: ErrorCodes.LessThan,
-                        rule: $"{Emitter.ValueParameterName} >= {value}",
-                        message: nameof(ErrorMessages) + "." + nameof(ErrorMessages.LessThan),
-                        placeholders: (ValidationPlaceholders.ComparisonValue, value));
-                    break;
-                case nameof(ITypelyBuilderOfInt.LessThanOrEqualTo):
-                    value = invocation.GetFirstNumberArgument();
-                    AddRule(
-                        errorCode: ErrorCodes.LessThanOrEqualTo,
-                        rule: $"{Emitter.ValueParameterName} > {value}",
-                        message: nameof(ErrorMessages) + "." + nameof(ErrorMessages.LessThanOrEqualTo),
-                        placeholders: (ValidationPlaceholders.ComparisonValue, value));
-                    break;
-                case nameof(ITypelyBuilderOfInt.NotEqual):
-                    value = invocation.GetFirstNumberArgument();
-                    AddRule(
-                        errorCode: ErrorCodes.NotEqual,
-                        rule: $"{Emitter.ValueParameterName}.Equals({value})",
-                        message: nameof(ErrorMessages) + "." + nameof(ErrorMessages.NotEqual),
-                        placeholders: (ValidationPlaceholders.ComparisonValue, value));
-                    break;
-                default: throw new NotSupportedException(invocation.MemberName);
-            }
+            
+            throw new NotSupportedException(invocation.MemberName);
         }
 
         return EmittableType;
