f8398a7a06318fa65bbbcd043f6d752300fdc605
Adam Paquette <nfs120@gmail.com>
2023-04-22 11:31:05 -0400

    Squashed commit of the following:
    commit 0ae9bdd2e92facb33bf2686bb7b7e253b15d9cd1
    Author: Adam Paquette <41346416+adampaquette@users.noreply.github.com>
    Date:   Sat Apr 22 11:21:25 2023 -0400
    
        Add sample project (#15)

---
+++ sample/Sample.Domain/ContactAggregate/TypelyConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+using Typely.Core;
+using Typely.Core.Builders;
+
+namespace Sample.Domain.ContactAggregate;
+
+internal class TypelyConfiguration : ITypelyConfiguration
+{
+    public void Configure(ITypelyBuilder builder)
+    {
+        builder.OfInt().For("ContactId");
+        builder.OfString().For("FirstName").MaxLength(100);
+        builder.OfString().For("LastName").MaxLength(100);
+        builder.OfString().For("Phone").MaxLength(12).Matches(new Regex("[0-9]{3}-[0-9]{3}-[0-9]{4}"));
+        builder.OfString().For("Street").MaxLength(100);
+        builder.OfString().For("City").MaxLength(50);
+        builder.OfString().For("State").MaxLength(50);
+        builder.OfString().For("ZipCode").Matches(new Regex(@"^((\d{5}-\d{4})|(\d{5})|([A-Z|a-z]\d[A-Z|a-z]\d[A-Z|a-z]\d))$"));
+    }
+}
