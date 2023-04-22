0ae9bdd2e92facb33bf2686bb7b7e253b15d9cd1
Adam Paquette <41346416+adampaquette@users.noreply.github.com>
2023-04-22 11:21:25 -0400

    Add sample project (#15)

---
+++ sample/Sample.Domain/ContactAggregate/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace Sample.Domain.ContactAggregate;
+
+public class Contact
+{
+    public ContactId Id { get; set; }
+    public FirstName? FirstName { get; set; }
+    public LastName? LastName { get; set; }
+    public Phone? Phone { get; set; }
+    public Street? Street { get; set; }
+    public City? City { get; set; }
+    public State? State { get; set; }
+    public ZipCode? ZipCode { get; set; }
+}
