cd692c0593e3900a661bc1ea707607e71aa09a37
Adam Paquette <nfs120@gmail.com>
2023-05-04 19:09:29 -0400

    equalitytests

---
+++ tests/Typely.Generators.Tests/Typely/Parsing/EmittableRuleTests.cs
@@ -0,0 +1,18 @@
+﻿using Typely.Generators.Typely.Parsing;
+
+namespace Typely.Generators.Tests.Typely.Parsing;
+
+public class EmittableRuleTests
+{
+    [Fact]
+    public void TwoRulesWithTheSameProperties_ShouldBe_Equal()
+    {
+        var rule1 = new EmittableRule("string", "Name", "Name",
+            new Dictionary<string, object?>() { { "MaxLength", 10 } });
+
+        var rule2 = new EmittableRule("string", "Name", "Name",
+            new Dictionary<string, object?>() { { "MaxLength", 10 } });
+
+        Assert.Equal(rule1, rule2);
+    }
+}
