958c700a95e94b935e768e4e261633cdba2d6246
Adam Paquette <nfs120@gmail.com>
2023-05-09 22:35:42 -0400

    test

--- tests/Typely.Tests/TypelyOptionsTests.cs
+++ tests/Typely.Tests/TypelyOptionsTests.cs
@@ -4,18 +4,6 @@ namespace Typely.Tests;
 
 public class TypelyOptionsTests
 {
-    [Fact(Skip = "Impact other tests")]
-    public void EnableSensitiveDataLogging_Should_OutputCurrentValue()
-    {
-        var expectedValue = 0;
-
-        TypelyOptions.Instance.EnableSensitiveDataLogging();
-        var validationError = TypelyOptionTestsType.Validate(expectedValue)!;
-        TypelyOptions.Instance.EnableSensitiveDataLogging(false);
-
-        Assert.Equal(expectedValue.ToString(), validationError.PlaceholderValues[ValidationPlaceholders.Value]);
-    }
-    
     [Fact]
     public void DisabledSensitiveDataLogging_ShouldNot_OutputCurrentValue()
     {
