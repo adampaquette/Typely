80ebf0484bcfc09c43a5a645d1fc53adce92255e
Adam Paquette <nfs12@gmail.com>
2023-10-03 22:21:16 -0400

    Benchmark read and serialization

--- benchmarks/Typely.Benchmarks/Program.cs
+++ benchmarks/Typely.Benchmarks/Program.cs
@@ -1,7 +1,8 @@
 ﻿using BenchmarkDotNet.Running;
 using Typely.Benchmarks;
 
-BenchmarkRunner.Run<ValueObjectLibrariesBenchmark.Int>();
+BenchmarkRunner.Run<ValueObjectLibrariesBenchmark.IntSerialization>();
+//BenchmarkRunner.Run<ValueObjectLibrariesBenchmark.IntRead>();
 //BenchmarkRunner.Run<ErrorBenchmark>();
 //BenchmarkRunner.Run<EqualsBenchmark.GuidTests>();
 //BenchmarkRunner.Run(typeof(Program).Assembly);
--- benchmarks/Typely.Benchmarks/ValueObjectLibrariesBenchmark.cs
+++ benchmarks/Typely.Benchmarks/ValueObjectLibrariesBenchmark.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using System.Text.Json;
 using Typely.Core;
 using Typely.Core.Builders;
 using ValueOf;
@@ -9,7 +10,40 @@ namespace Typely.Benchmarks;
 public class ValueObjectLibrariesBenchmark
 {
     [MemoryDiagnoser]
-    public class Int
+    public class IntRead
+    {
+        private TypelyInt typelyInt = TypelyInt.From(1);
+        private ValueOfInt valueOfInt = ValueOfInt.From(1);
+        private VogenInt vogenInt = VogenInt.From(1);
+        private int primitiveInt = 1;
+
+        [Benchmark]
+        public void IntRead_Typely()
+        {
+            int i = typelyInt.Value;
+        }
+
+        [Benchmark]
+        public void IntRead_ValueOf()
+        {
+            int i = valueOfInt.Value;
+        }
+
+        [Benchmark]
+        public void IntRead_Vogen()
+        {
+            int i = vogenInt.Value;
+        }
+
+        [Benchmark]
+        public void IntRead_Primitive()
+        {
+            int i = primitiveInt;
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class IntCreation
     {
         [Benchmark]
         public void IntCreation_Typely()
@@ -28,7 +62,7 @@ public class ValueObjectLibrariesBenchmark
         {
             VogenInt.From(1);
         }
-        
+
         [Benchmark]
         public void IntCreation_Primitive()
         {
@@ -41,6 +75,43 @@ public class ValueObjectLibrariesBenchmark
             {
                 throw new Exception();
             }
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class IntSerialization
+    {
+        private static TypelyInt typelyInt = TypelyInt.From(1);
+        private static ValueOfInt valueOfInt = ValueOfInt.From(1);
+        private static VogenInt vogenInt = VogenInt.From(1);
+        private static int primitiveInt = 1;
+
+        [Benchmark]
+        public void IntSerialization_Typely()
+        {
+            var json = JsonSerializer.Serialize(typelyInt);
+            var deserialized = JsonSerializer.Deserialize<TypelyInt>(json);
+        }
+
+        [Benchmark]
+        public void IntSerialization_ValueOf()
+        {
+            var json = JsonSerializer.Serialize(valueOfInt);
+            var deserialized = JsonSerializer.Deserialize<ValueOfInt>(json);
+        }
+
+        [Benchmark]
+        public void IntSerialization_Vogen()
+        {
+            var json = JsonSerializer.Serialize(vogenInt);
+            var deserialized = JsonSerializer.Deserialize<VogenInt>(json);
+        }
+
+        [Benchmark]
+        public void IntSerialization_Primitive()
+        {
+            var json = JsonSerializer.Serialize(primitiveInt);
+            var deserialized = JsonSerializer.Deserialize<int>(json);
         }
     }
 }
@@ -68,6 +139,5 @@ public class TypelySpec : ITypelySpecification
     public void Create(ITypelyBuilder builder)
     {
         builder.OfInt().For("TypelyInt").GreaterThan(0);
-        builder.OfInt().For("TypelyInt2").GreaterThan(0);
     }
 }
