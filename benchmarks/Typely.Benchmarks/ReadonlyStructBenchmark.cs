61e8438ebcb16a965b5ae351e666499d3f198b49
Adam Paquette <nfs12@gmail.com>
2024-06-09 00:50:41 -0400

    benchmark

--- benchmarks/Typely.Benchmarks/ReadonlyStructBenchmark.cs
+++ benchmarks/Typely.Benchmarks/ReadonlyStructBenchmark.cs
@@ -1,64 +1,37 @@
 ﻿using BenchmarkDotNet.Attributes;
-using System.Runtime.CompilerServices;
 
 namespace Typely.Benchmarks;
 
 [MemoryDiagnoser]
 public class ReadonlyStructBenchmark
 {
-    private struct Point
+    private struct NumberStruct(int value)
     {
-        public int X { get; private set ; }
-        public int Y { get; private set; }
+        public int Value { get; } = value;
 
-        public Point(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
+        public override string ToString() => Value.ToString();
     }
 
-    private readonly struct ReadOnlyPoint
+    private readonly struct NumberReadonlyStruct(int value)
     {
-        public int X { get; }
-        public int Y { get; }
+        public int Value { get; } = value;
 
-        public ReadOnlyPoint(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
+        public override string ToString() => Value.ToString();
     }
-    
-    [Params(10, 100)]
-    public int X;
 
-    [Params(10, 100)]
-    public int Y;
-    
+    private readonly NumberStruct _numberStruct = new(1);
+    private readonly NumberReadonlyStruct _numberReadonlyStruct = new(1);
+
     [Benchmark]
-    public int Struct()
-    {
-        var point = new Point(X, Y);
-        return Sum(point);
+    public string Struct()
+    { 
+        // Making a defensive copy of the value
+        return _numberStruct.ToString();
     }
 
     [Benchmark]
-    public int ReadOnlyStruct()
+    public string ReadOnlyStruct()
     {
-        var point = new ReadOnlyPoint(X, Y);
-        return Sum(point);
-    }
-    
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    private int Sum(Point point)
-    {
-        return point.X + point.Y;
-    }
-    
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    private int Sum(ReadOnlyPoint point)
-    {
-        return point.X + point.Y;
+        return _numberReadonlyStruct.ToString();
     }
 }
